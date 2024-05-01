07910be1d818ad71947f0afb52008eaa5a5ae616
Yonatan Leulseged <82597472+Yonatan-oni@users.noreply.github.com>
2024-05-01 13:36:51 +0300

    Time: 2395 ms (5.16%), Space: 44.9 MB (89.15%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int number = 0;
+        int counter = 0;
+        List<Integer> notAnswer = new ArrayList();
+
+        for(int i =0; i < nums.length; i++){
+            counter = 0;
+            for(int j=0; j < nums.length; j++){
+               while(counter<(nums.length)){
+                if( i != j && nums[i] == nums[j])
+                    notAnswer.add(nums[j]);
+                j++;
+                counter++;
+               }
+            }
+        }
+
+        for(int i = 0; i < nums.length; i++){
+            if(!notAnswer.contains(nums[i]))
+                number = nums[i];
+        }
+
+        return number;
+    }
+}
